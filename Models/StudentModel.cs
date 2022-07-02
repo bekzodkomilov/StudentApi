e98408a50925db548cf3aa9ded3c38521b51bea6
bekzodkomilov <komilovbek1998@gmail.com>
2022-07-03 01:42:34 +0500

    Date

---
+++ Models/StudentModel.cs
@@ -0,0 +1,9 @@
+namespace StudentApi.Models;
+
+public class StudentModel
+{
+    public Guid Id { get; set; }
+    public string LastName { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
