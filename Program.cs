df6d7bb41caa73e09ed46b49d50b046afbf7cf02
bekzodkomilov <komilovbek1998@gmail.com>
2022-07-04 21:53:31 +0500

    comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using System.Reflection.Metadata;
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using StudentApi.Data;
@@ -6,13 +5,11 @@ using StudentApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StudentDbContext>(options =>
@@ -24,7 +21,6 @@ builder.Services.AddSingleton<StudentService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
